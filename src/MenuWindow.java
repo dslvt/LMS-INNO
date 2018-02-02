ea2fac15d887cec6b062082cccdd6792439e6117
Enes Ayan <eayan2012@litsey2.ru>
2018-02-02 12:50:57 +0300

    Added: - Some classes

---
+++ src/MenuWindow.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MenuWindow extends JFrame {
+    private JButton takeBook = new JButton("Take Book");
+    private JButton myBooks = new JButton("My Books");
+    private JButton returnBook = new JButton("Return Book");
+
+    public MenuWindow(){
+        JFrame menuWindow = new JFrame();
+        menuWindow.setBounds(100, 100, 250, 150);
+        menuWindow.setLocationRelativeTo(null);
+        menuWindow.setResizable(false);
+        menuWindow.setTitle("Sign Up");
+        menuWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container containerM = menuWindow.getContentPane();
+        containerM.setLayout(new GridLayout(6, 2, 2, 2));
+        containerM.add(takeBook);
+        containerM.add(myBooks);
+        containerM.add(returnBook);
+        takeBook.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TakeBook books = new TakeBook();
+            }
+        });
+        menuWindow.setVisible(true);
+    }
+}
