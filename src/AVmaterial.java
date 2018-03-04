ebed7b73f15f036a169ca6b1ba3b2365a35f16a7
arina <arina.fedorovskaya@gmail.com>
2018-03-04 14:15:23 +0300

    Implement addCopies and deleteCopies for AV Material and Journal, changed CreateDocumentInDB and ModifyInDB for AV Material and Journal

--- src/AVmaterial.java
+++ src/AVmaterial.java
@@ -44,10 +44,7 @@ public class AVmaterial extends Document {
 
             try {
                 Integer lastId = Database.isDocumentExist(this);
-                if (lastId != -1) {
-                    preparedStatement = Database.connection.prepareStatement("update av_materials set number = number + 1 where id = " + lastId.toString());
-                    preparedStatement.executeUpdate();
-                } else {
+                if (lastId == -1) {
                     preparedStatement = Database.connection.prepareStatement("insert into av_materials(title, author, cost, keywords, number, reference) values(?, ?, ?, ?, ?, ?)");
                     preparedStatement.setString(1, this.name);
                     preparedStatement.setString(2, this.authors.toString());
@@ -63,21 +60,24 @@ public class AVmaterial extends Document {
                         lastId = resultSet.getInt(1);
                     }
                     this.localId = lastId;
-                }
 
-                preparedStatement = Database.connection.prepareStatement("insert into documents(id_av_materials, location, type) values(?, ?, ?)");
-                preparedStatement.setInt(1, lastId);
-                preparedStatement.setString(2, location);
-                preparedStatement.setString(3, "av_materials");
-                preparedStatement.executeUpdate();
+                    preparedStatement = Database.connection.prepareStatement("insert into documents(id_av_materials, location, type) values(?, ?, ?)");
+                    preparedStatement.setInt(1, lastId);
+                    preparedStatement.setString(2, location);
+                    preparedStatement.setString(3, "av_materials");
+                    preparedStatement.executeUpdate();
 
-                int globalID = 0;
-                statement = Database.connection.createStatement();
-                resultSet = statement.executeQuery("SELECT LAST_INSERT_ID();");
-                if (resultSet.next()) {
-                    globalID = resultSet.getInt(1);
+                    int globalID = 0;
+                    statement = Database.connection.createStatement();
+                    resultSet = statement.executeQuery("SELECT LAST_INSERT_ID();");
+                    if (resultSet.next()) {
+                        globalID = resultSet.getInt(1);
+                    }
+                    this.id = globalID;
                 }
-                this.id = globalID;
+                else {
+                    System.out.println("Error create AV Material: this AV material is already exist");
+                }
 
             } catch (Exception e) {
                 System.out.println("Error create av_material: " + e.toString());
@@ -87,13 +87,76 @@ public class AVmaterial extends Document {
         }
     }
 
-    public void ModifyInDB(String name, ArrayList<String> authors, int cost, ArrayList<String> keywords, boolean isReference, String location, boolean isActive, int idLibrarian) {
+    public ArrayList<AVmaterial> addCopies(int copies, int idLibrarian) {
+        ArrayList<AVmaterial> newCopies = new ArrayList<>();
         if (Database.isLibrarian(idLibrarian)) {
-            AVmaterial modifiedAV = new AVmaterial(name, authors, cost, keywords, isReference, isActive, location);
-            modifiedAV.id = this.id;
-            modifiedAV.localId = this.localId;
-            this.DeleteFromDB(idLibrarian);
-            modifiedAV.CreateDocumentInDB(idLibrarian);
+            PreparedStatement preparedStatement;
+            Statement statement;
+            ResultSet resultSet;
+            Integer lastId = Database.isDocumentExist(this);
+            if (lastId != -1) {
+                try {
+                    for (int i = 0; i < copies; i++) {
+                        preparedStatement = Database.connection.prepareStatement("insert into av_materials(title, author, cost, keywords, number, reference) values(?, ?, ?, ?, ?, ?)");
+                        preparedStatement.setString(1, this.name);
+                        preparedStatement.setString(2, this.authors.toString());
+                        preparedStatement.setInt(3, price);
+                        preparedStatement.setString(4, this.keywords.toString());
+                        preparedStatement.setInt(5, 1);
+                        preparedStatement.setBoolean(6, this.isReference);
+                        preparedStatement.executeUpdate();
+
+                        int globalID = 0;
+                        statement = Database.connection.createStatement();
+                        resultSet = statement.executeQuery("SELECT LAST_INSERT_ID();");
+                        if (resultSet.next()) {
+                            globalID = resultSet.getInt(1);
+                        }
+
+                        AVmaterial newCopy = this;
+                        newCopy.id = globalID;
+                        newCopies.add(newCopy);
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Error add copies of  AV material: " + e.toString());
+                }
+            } else {
+                System.out.println("Error add copy of AV material:  there is no AV material with id " + lastId.toString());
+            }
+        } else {
+            System.out.println("Error: User does not have access to add new copy of AV Material");
+        }
+
+        return newCopies;
+    }
+
+    public void ModifyInDB(String name, ArrayList<String> authors, int cost, ArrayList<String> keywords, boolean isReference, String location, int idLibrarian) {
+        if (Database.isLibrarian(idLibrarian)) {
+            PreparedStatement preparedStatement;
+            try {
+                Integer lastId = Database.isDocumentExist(this);
+                if (lastId != -1) {
+                    preparedStatement = Database.connection.prepareStatement("UPDATE av_materials SET title = ?, author = ?, cost = ?, keywords = ?, reference = ? WHERE id = ?");
+                    preparedStatement.setString(1, name);
+                    preparedStatement.setString(2, authors.toString());
+                    preparedStatement.setInt(3, cost);
+                    preparedStatement.setString(4, keywords.toString());
+                    preparedStatement.setBoolean(5, isReference);
+                    preparedStatement.setInt(6,this.localId);
+                    preparedStatement.executeUpdate();
+
+                    preparedStatement = Database.connection.prepareStatement("UPDATE documents SET location = ? WHERE id = ?");
+                    preparedStatement.setString(1, location);
+                    preparedStatement.setInt(2, this.id);
+                    preparedStatement.executeUpdate();
+                } else {
+                    System.out.println("Error modify AV material: there is no AV material with id " + lastId.toString());
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Error modify AV material: " + e.toString());
+            }
+
         } else {
             System.out.println("Error: User does not have access to modify AV Material");
         }
@@ -122,6 +185,22 @@ public class AVmaterial extends Document {
             System.out.println("Error: User does not have access to delete AV Material");
         }
 
+    }
+    public void deleteCopies(int copies, int idLibrarian) {
+        if (Database.isLibrarian(idLibrarian)) {
+            Database db = new Database();
+            Statement statement;
+            try {
+                statement = db.connection.createStatement();
+                Integer lastId = Database.isDocumentExist(this);
+                statement.executeUpdate("DELETE FROM documents WHERE id_av_materials= " + lastId.toString() + " LIMIT " + copies);
+
+            } catch (SQLException e) {
+                System.out.println("Error delete copy of AV Material: " + e.toString());
+            }
+        } else {
+            System.out.println("Error: User does not have access to delete copies of AV Material");
+        }
     }
 
     @Override
--- src/Journal.java
+++ src/Journal.java
@@ -1,6 +1,7 @@
 import javax.xml.crypto.Data;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
@@ -35,10 +36,7 @@ public class Journal extends Document {
 
             try {
                 Integer lastId = Database.isDocumentExist(this);
-                if (lastId != -1) {
-                    preparedStatement = Database.connection.prepareStatement("update journals set number = number + 1 where id = " + lastId.toString());
-                    preparedStatement.executeUpdate();
-                } else {
+                if (lastId == -1) {
                     preparedStatement = Database.connection.prepareStatement("insert into journals(title, author, issue, editor, cost, keywords, reference, number) values(?, ?, ?, ?, ?, ?, ?, ?)");
                     preparedStatement.setString(1, this.name);
                     preparedStatement.setString(2, this.authors.toString());
@@ -56,21 +54,24 @@ public class Journal extends Document {
                         lastId = resultSet.getInt(1);
                     }
                     this.localId = lastId;
-                }
 
-                preparedStatement = Database.connection.prepareStatement("insert into documents(id_journals, location, type) values(?, ?, ?)");
-                preparedStatement.setInt(1, lastId);
-                preparedStatement.setString(2, location);
-                preparedStatement.setString(3, "journals");
-                preparedStatement.executeUpdate();
+                    preparedStatement = Database.connection.prepareStatement("insert into documents(id_journals, location, type) values(?, ?, ?)");
+                    preparedStatement.setInt(1, lastId);
+                    preparedStatement.setString(2, location);
+                    preparedStatement.setString(3, "journals");
+                    preparedStatement.executeUpdate();
 
-                int globalID = 0;
-                statement = Database.connection.createStatement();
-                resultSet = statement.executeQuery("SELECT LAST_INSERT_ID();");
-                if (resultSet.next()) {
-                    globalID = resultSet.getInt(1);
+                    int globalID = 0;
+                    statement = Database.connection.createStatement();
+                    resultSet = statement.executeQuery("SELECT LAST_INSERT_ID();");
+                    if (resultSet.next()) {
+                        globalID = resultSet.getInt(1);
+                    }
+                    this.id = globalID;
                 }
-                this.id = globalID;
+                else{
+                    System.out.println("Error create journal: this journal is already exist");
+                }
 
             } catch (Exception e) {
                 System.out.println("Error create journal: " + e.toString());
@@ -80,14 +81,80 @@ public class Journal extends Document {
         }
     }
 
-    public void ModifyInDB(String name, ArrayList<String> authors, int cost, ArrayList<String> keywords,
-                           boolean isReference, String publicationDate, String issue, String editor, boolean isActive, String location, int idLibrarian) {
+    public ArrayList<Journal> addCopies(int copies, int idLibrarian) {
+        ArrayList<Journal> newCopies = new ArrayList<>();
         if (Database.isLibrarian(idLibrarian)) {
-            Journal modifiedJournal = new Journal(name, authors, cost, keywords, isReference, publicationDate, issue, editor, isActive, location);
-            modifiedJournal.id = this.id;
-            modifiedJournal.localId = this.localId;
-            this.DeleteFromDB(idLibrarian);
-            modifiedJournal.CreateDocumentInDB(idLibrarian);
+            PreparedStatement preparedStatement;
+            Statement statement;
+            ResultSet resultSet;
+            Integer lastId = Database.isDocumentExist(this);
+            if (lastId != -1) {
+                try {
+                    for (int i = 0; i < copies; i++) {
+                        preparedStatement = Database.connection.prepareStatement("update journals set number = number + 1 where id = " + lastId.toString());
+                        preparedStatement.executeUpdate();
+
+                        preparedStatement = Database.connection.prepareStatement("insert into documents(id_journals, location, type) values(?, ?, ?)");
+                        preparedStatement.setInt(1, lastId);
+                        preparedStatement.setString(2, location);
+                        preparedStatement.setString(3, "journals");
+                        preparedStatement.executeUpdate();
+
+                        int globalID = 0;
+                        statement = Database.connection.createStatement();
+                        resultSet = statement.executeQuery("SELECT LAST_INSERT_ID();");
+                        if (resultSet.next()) {
+                            globalID = resultSet.getInt(1);
+                        }
+
+                        Journal newCopy = this;
+                        newCopy.id = globalID;
+                        newCopies.add(newCopy);
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Error add copies of  journal: " + e.toString());
+                }
+            } else {
+                System.out.println("Error add copy of journal:  there is no journal with id " + lastId.toString());
+            }
+        } else {
+            System.out.println("Error: User does not have access to add new copy of Journal");
+        }
+
+        return newCopies;
+    }
+
+    public void ModifyInDB(String name, ArrayList<String> authors, int cost, ArrayList<String> keywords,
+                           boolean isReference, String publicationDate, String issue, String editor, String location, int idLibrarian) {
+        if (Database.isLibrarian(idLibrarian)) {
+            PreparedStatement preparedStatement;
+            try {
+                Integer lastId = Database.isDocumentExist(this);
+                if (lastId != -1) {
+                    preparedStatement = Database.connection.prepareStatement("UPDATE journals SET title = ?, author = ?, issue = ?, editor = ?, cost = ?, keywords = ?, reference = ?, publicationDate = ? WHERE id = ?");
+                    preparedStatement.setString(1, name);
+                    preparedStatement.setString(2, authors.toString());
+                    preparedStatement.setString(3, issue);
+                    preparedStatement.setString(4, editor);
+                    preparedStatement.setInt(5, cost);
+                    preparedStatement.setString(6, keywords.toString());
+                    preparedStatement.setBoolean(7, isReference);
+                    preparedStatement.setString(8,publicationDate);
+                    preparedStatement.setInt(9, this.localId);
+                    preparedStatement.executeUpdate();
+
+                    preparedStatement = Database.connection.prepareStatement("UPDATE documents SET location = ? WHERE id = ?");
+                    preparedStatement.setString(1, location);
+                    preparedStatement.setInt(2, this.id);
+                    preparedStatement.executeUpdate();
+                } else {
+                    System.out.println("Error modify journal:  there is no journal with id " + lastId.toString());
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Error modify journal: " + e.toString());
+            }
+
         } else {
             System.out.println("Error: User does not have access to modify Journal");
         }
@@ -104,6 +171,7 @@ public class Journal extends Document {
                 if (lastId != -1) {
                     statement.executeUpdate("DELETE FROM journals WHERE id = " + lastId.toString());
                     statement.executeUpdate("DELETE FROM documents WHERE id = " + this.id);
+                    statement.executeUpdate("DELETE FROM  journal_articles WHERE  journal_id = " + this.localId);
 
                 } else {
                     System.out.println("Error delete journal:  there is no journal with id " + lastId.toString());
@@ -114,6 +182,23 @@ public class Journal extends Document {
             }
         } else {
             System.out.println("Error: User does not have access to delete Journal");
+        }
+    }
+
+    public void deleteCopies(int copies, int idLibrarian) {
+        if (Database.isLibrarian(idLibrarian)) {
+            Database db = new Database();
+            Statement statement;
+            try {
+                statement = db.connection.createStatement();
+                Integer lastId = Database.isDocumentExist(this);
+                statement.executeUpdate("DELETE FROM documents WHERE id_journals= " + lastId.toString() + " LIMIT " + copies);
+
+            } catch (SQLException e) {
+                System.out.println("Error delete copy of journal: " + e.toString());
+            }
+        } else {
+            System.out.println("Error: User does not have access to delete copies of Journal");
         }
     }
 
