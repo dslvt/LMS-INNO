a2cf3600a4f1b481d2a5bc9c3f137abd00f7ab41
arina <arina.fedorovskaya@gmail.com>
2018-03-24 17:52:01 +0300

    Request

---
+++ src/Request.java
@@ -0,0 +1,10 @@
+public class Request {
+    Document document;
+    String message;
+
+    Request(Document document, String message){
+        this.document = document;
+        this.message = message;
+    }
+
+}
