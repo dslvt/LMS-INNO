20266e0d0c8b3d2f0ff3437837a202209490e3e2
eazykyube <eayan2012@litsey2.ru>
2018-03-28 15:16:11 -0700

    LibrarianDocumentGUI 1) added list 2) changed actionListener for edit book button

--- src/Database.java
+++ src/Database.java
@@ -14,8 +14,10 @@ public class Database {
 //    private static final String password = "FJ`;62LfOTVZoM2+;3Qo983_zq9iGix9S107pi6)|CzU2`rdVRZD7?5a65sM;|6'54FE\\w9t4Ph~=";
     //private static final String password = "333999333tima";
     String user = "root";
-    String password = "enaca2225";
-    String url = "jdbc:mysql://localhost:3306/project?useSSL=false";
+//    String password = "enaca2225";
+//    String url = "jdbc:mysql://localhost:3306/project?useSSL=false";
+    String password = "123123123Aa";
+    String url = "jdbc:mysql://localhost:3306/db?useSSL=false";
 
     public static Connection connection;
     private static Statement statement;
--- src/LibrarianDocumentGUI.java
+++ src/LibrarianDocumentGUI.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 class LibrarianDocumentGUI extends JFrame{
     private JButton EditBook = new JButton("EditBook");
@@ -12,17 +13,43 @@ class LibrarianDocumentGUI extends JFrame{
 
     public LibrarianDocumentGUI(){
         JFrame menuWindow = new JFrame();
-        menuWindow.setBounds(100, 100, 250, 250);
+        menuWindow.setBounds(100, 100, 250, 385);
         menuWindow.setLocationRelativeTo(null);
         menuWindow.setResizable(false);
         menuWindow.setTitle("Librarian");
         Container containerM = menuWindow.getContentPane();
-        containerM.setLayout(new GridLayout(5, 1, 2, 2));
+        containerM.setLayout(new FlowLayout());
+
+        ArrayList<Document> documents = Database.getAllDocuments();
+
+        Object[][] books = new Object[documents.size()][];
+
+        for (int i = 0; i < documents.size(); i++) {
+            books[i] = new Object[4];
+            books[i][0] = documents.get(i).name;
+            books[i][1] = documents.get(i).authors;
+            books[i][2] = documents.get(i).location;
+            books[i][3] = documents.get(i).price;
+        }
+
+        String[] columnNames = {"Name", "Authors", "Location", "Price"};
+
+        JTable table = new JTable(books, columnNames);
+        JScrollPane listScroller = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+        listScroller.setPreferredSize(new Dimension(240,118));
+        containerM.add(listScroller);
+        EditBook.setPreferredSize(new Dimension(240, 40));
         containerM.add(EditBook);
+        DeleteBook.setPreferredSize(new Dimension(240, 40));
         containerM.add(DeleteBook);
+        AddBook.setPreferredSize(new Dimension(240, 40));
         containerM.add(AddBook);
+        Create.setPreferredSize(new Dimension(240, 40));
         containerM.add(Create);
+        AllDocuments.setPreferredSize(new Dimension(240, 40));
         containerM.add(AllDocuments);
+
 //111
         AllDocuments.addActionListener(new ActionListener() {
             @Override
@@ -41,7 +68,20 @@ class LibrarianDocumentGUI extends JFrame{
         EditBook.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-                EditDocumentGUI books = new EditDocumentGUI();
+                int index = table.getSelectedRow();
+                if(index > 0) {
+                    if (documents.get(index).type == DocumentType.book) {
+                        AddBookGUI book = new AddBookGUI();
+                    } else if (documents.get(index).type == DocumentType.journal) {
+                        AddJournalGUI journal = new AddJournalGUI();
+                    } else if (documents.get(index).type == DocumentType.avmaterial) {
+                        AddAVmaterialGUI AVmaterial = new AddAVmaterialGUI();
+                    }
+                }
+                else{
+                    String message = "No row is selected\n" + "You need to select one to edit";
+                    JOptionPane.showMessageDialog(null, message, "ERROR", JOptionPane.PLAIN_MESSAGE);
+                }
             }
         });
 
