7a8f7898ab76d4adee72f2c28c213e68fac27e13
eazykyube <eayan2012@litsey2.ru>
2018-04-23 13:56:37 -0700

    AllActions

--- src/LastActionsGUI.java
+++ src/LastActionsGUI.java
@@ -1,4 +1,33 @@
+import com.mysql.jdbc.log.Log;
+
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class LastActionsGUI extends JFrame {
+    private Logging logging = new Logging();
+    private JTable table;
+
+    public LastActionsGUI(int number){
+        JFrame menuWindow = new JFrame();
+        menuWindow.setBounds(100, 100, 310, 310);
+        menuWindow.setLocationRelativeTo(null);
+        menuWindow.setResizable(false);
+        menuWindow.setTitle("Last actions");
+        menuWindow.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        Container containerM = menuWindow.getContentPane();
+        containerM.setLayout(new FlowLayout());
+        ArrayList<String> lastActions = Logging.getLast(number);
+        Object[][] obj = new Object[lastActions.size()][0];
+        for (int i=0; i< lastActions.size(); i++)
+            obj[i][0] = lastActions.get(i);
+        Object[] str = {"Actions"};
+        table = new JTable(obj, str);
+        JScrollPane listScroller = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        listScroller.setPreferredSize(new Dimension(300, 300));
+        containerM.add(listScroller);
+        menuWindow.setVisible(true);
+    }
 }
--- src/LibrarianGUI.java
+++ src/LibrarianGUI.java
@@ -1,8 +1,26 @@
+import com.mysql.jdbc.log.Log;
+
 import javax.swing.*;
 import javax.xml.crypto.Data;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
 
 class LibrarianGUI extends JFrame{
     private JButton Books = new JButton("Books");
@@ -11,6 +29,7 @@ class LibrarianGUI extends JFrame{
     private JButton Request = new JButton("Checked out documents");
     private JButton LastActions = new JButton("Last actions");
     private JButton logOut = new JButton("Log out");
+    private IntTextField intTextField;
 
     public LibrarianGUI(int user_id){
         JFrame menuWindow = new JFrame();
@@ -28,8 +47,13 @@ class LibrarianGUI extends JFrame{
         containerM.add(Users);
         containerM.add(Tasks);
         containerM.add(Request);
-        if(Database.isAdmin(user_id))
-            containerM.add(LastActions);
+        if(Database.isAdmin(user_id)){
+            Panel panel = new Panel(new GridLayout(1, 2, 2, 2));
+            intTextField = new IntTextField(10, 3);
+            panel.add(intTextField);
+            panel.add(LastActions);
+            containerM.add(panel);
+        }
         containerM.add(logOut);
 
         Books.addActionListener(new ActionListener() {
@@ -63,7 +87,14 @@ class LibrarianGUI extends JFrame{
         LastActions.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                LastActionsGUI lastActionsGUI = new LastActionsGUI();
+                int number = intTextField.getValue();
+                if(number == 0) {
+                    String message = "Enter a number!\n";
+                    JOptionPane.showMessageDialog(null, message, "ERROR", JOptionPane.PLAIN_MESSAGE);
+                }
+                else{
+                    LastActionsGUI lastActionsGUI = new LastActionsGUI(number);
+                }
             }
         });
 
@@ -79,6 +110,48 @@ class LibrarianGUI extends JFrame{
                 restartFirstWindow.setVisible(true);
             }
         });
+
         menuWindow.setVisible(true);
+    }
+}
+class IntTextField extends JTextField {
+    public IntTextField(int defval, int size) {
+        super("" + defval, size);
+    }
+
+    protected Document createDefaultModel() {
+        return new IntTextDocument();
+    }
+
+    public boolean isValid() {
+        try {
+            Integer.parseInt(getText());
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public int getValue() {
+        try {
+            return Integer.parseInt(getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+    class IntTextDocument extends PlainDocument {
+        public void insertString(int offs, String str, AttributeSet a)
+                throws BadLocationException {
+            if (str == null)
+                return;
+            String oldString = getText(0, getLength());
+            String newString = oldString.substring(0, offs) + str
+                    + oldString.substring(offs);
+            try {
+                Integer.parseInt(newString + "0");
+                super.insertString(offs, str, a);
+            } catch (NumberFormatException e) {
+            }
+        }
     }
 }
