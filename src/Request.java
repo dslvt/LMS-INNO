92c98e8945e69e85ee61624a35a83a9bcfea31b7
arina <arina.fedorovskaya@gmail.com>
2018-04-25 15:53:22 +0300

    Comments

--- src/Request.java
+++ src/Request.java
@@ -2,6 +2,12 @@ public class Request {
     Document document;
     String message;
 
+    /**
+     * Constructor to create request
+     * @param document Document on which request is send
+     * @param message Message which is shown to user
+     */
+
     Request(Document document, String message){
         this.document = document;
         this.message = message;
--- src/UserRequest.java
+++ src/UserRequest.java
@@ -2,6 +2,10 @@ public class UserRequest {
     Patron patron;
     Document document;
 
+    /**
+     * Used to create UserRequest
+     */
+
     public UserRequest(Patron patron, Document document){
         this.patron = patron;
         this.document = document;
